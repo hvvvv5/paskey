c06ab311570d0b05614c7f183f931c60f7874a7d
hvvvv5 <github192026@gmail.com>
2026-09-21 14:55:20 +0100

    Convert Android insets to CSS pixels

--- android/app/src/main/java/com/paskey/vault/MainActivity.java
+++ android/app/src/main/java/com/paskey/vault/MainActivity.java
@@ -21,20 +21,28 @@ public class MainActivity extends BridgeActivity {
 
     private void installSystemBarInsets() {
         final WebView webView = getBridge().getWebView();
+        final float density = getResources().getDisplayMetrics().density;
+
         ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
             Insets insets = windowInsets.getInsets(
                 WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout()
             );
 
+            int topDp = Math.round(insets.top / density);
+            int rightDp = Math.round(insets.right / density);
+            int bottomDp = Math.round(insets.bottom / density);
+            int leftDp = Math.round(insets.left / density);
+
             String script =
-                "document.documentElement.style.setProperty('--pk-safe-top','" + insets.top + "px');" +
-                "document.documentElement.style.setProperty('--pk-safe-right','" + insets.right + "px');" +
-                "document.documentElement.style.setProperty('--pk-safe-bottom','" + insets.bottom + "px');" +
-                "document.documentElement.style.setProperty('--pk-safe-left','" + insets.left + "px');";
+                "document.documentElement.style.setProperty('--pk-safe-top','" + topDp + "px');" +
+                "document.documentElement.style.setProperty('--pk-safe-right','" + rightDp + "px');" +
+                "document.documentElement.style.setProperty('--pk-safe-bottom','" + bottomDp + "px');" +
+                "document.documentElement.style.setProperty('--pk-safe-left','" + leftDp + "px');";
 
             webView.post(() -> webView.evaluateJavascript(script, null));
             return windowInsets;
         });
+
         ViewCompat.requestApplyInsets(webView);
     }
 }
